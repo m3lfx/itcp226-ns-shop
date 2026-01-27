3789d3866c31235c3c3975d9ad7bbb2668385d62
rommel dalisay <rommelmdalisay@gmail.com>
2026-01-27 09:19:42 +0800

    add stock foreign key

---
+++ database/migrations/2026_01_27_011136_add_item_id_foreign_unique_to_stock_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('stock', function (Blueprint $table) {
+            //     $table->foreignId('item_id')
+            //         ->unique() // Must be placed before constrained()
+            //         ->constrained('item') // Assumes the referenced table is 'users'
+            //         ->onDelete('cascade')->change();
+            // });
+            $table->foreign('item_id')->references('item_id')->on('item')->onDelete('cascade')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('stock', function (Blueprint $table) {
+            $table->dropForeign(['item_id']);
+        });
+    }
+};
